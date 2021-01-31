3052c15e1de751f6eb98d4aec052bb23c1aee909
profesorek96 <krzysztof.los.dev@gmail.com>
2021-01-31 11:07:22 +0100

    Dodano model dla Pytnaia oraz dla urzytkownika, poprawiono szablon

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Pytanie,Urzytkownicy
+
+admin.site.register(Pytanie)
+admin.site.register(Urzytkownicy)
--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Pytanie(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    tresc=models.TextField()
+    odp_A=models.CharField(max_length=200)
+    odp_B = models.CharField(max_length=200)
+    odp_C = models.CharField(max_length=200)
+    odp_D = models.CharField(max_length=200)
+    odp_poprawna=models.CharField(max_length=200)
+
+
+class Urzytkownicy(models.Model):
+    imie = models.CharField(max_length=30)
+    nazwisko = models.CharField(max_length=30)
+    liczba_punktow=models.IntegerField()
